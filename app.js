033b31a955d972501569e5bdcafade695e7f1df6
Patrick <patrickmahinay031@gmail.com>
2023-03-17 10:21:33 +0800

    pooling

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const app = express();
 const { connection } = require("./db/connection");
 const port = process.env.PORT || 3001;
 
-connection.connect((err) => {
-  if (err) return console.error("error connecting: " + err.stack);
-  return console.log("connected as id " + connection.threadId);
+connection.on("acquire", (connection) => {
+  console.log("Connection %d acquired", connection.threadId);
 });
+
+connection.on("release", (connection) => {
+  console.log("Connection %d released", connection.threadId);
+});
+
+// Create Connection Action
+// connection.getConnection((err) => {
+//   if (err) return console.error("error connecting: " + err.stack);
+//   return console.log("connected as id " + connection.threadId);
+// });
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -25,6 +34,5 @@ app.use("/category/", CategoryRouter);
 app.use("/medicine/", MedicineRouter);
 app.use("/account/", AccountRouter);
 app.use("/activity/", AccountRouter);
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+  connectionLimit: 10,
   host: "db4free.net",
   user: "rams031",
   password: "0923596633",
   database: "medinventory",
 });
 
-module.exports = { connection }
+module.exports = { connection };
