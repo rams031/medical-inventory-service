e2b20fe12ad203d4eadfc1aa98cffb46835a1137
rams031 <patrick.sarado2@gmail.com>
2023-03-26 16:04:30 +0800

    new features

--- routes/barangay.js
+++ routes/barangay.js
@@ -4,7 +4,7 @@ const { connection } = require("./../db/connection");
 router.get("/", (req, res) => {
   return connection.query(
     {
-      sql: "SELECT * FROM `accounts`",
+      sql: "SELECT * FROM `barangay`",
     },
     function (error, results, fields) {
       if (error) return res.status(400).send(error);
@@ -12,4 +12,41 @@ router.get("/", (req, res) => {
     }
   );
 });
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params || {};
+
+  return connection.query(
+    {
+      sql: "SELECT * FROM `barangay` WHERE id=?",
+      values: [id],
+    },
+    function (error, results, fields) {
+      if (error) return res.status(400).send(error);
+      res.status(200).json(results);
+    }
+  );
+});
+
+router.post("/create", (req, res) => {
+  const { barangayLogo, barangayName, barangayDescription, barangayAddress } =
+    req?.body || {};
+
+  return connection.query(
+    {
+      sql: "INSERT INTO `barangay`( `barangayName`, `barangayLogo`, `barangayDescription`, `barangayAddress`) VALUES (?,?,?,?)",
+      values: [
+        barangayName,
+        barangayLogo,
+        barangayDescription,
+        barangayAddress,
+      ],
+    },
+    function (error, results, fields) {
+      if (error) return res.status(400).send(error);
+      res.status(200).json(results);
+    }
+  );
+});
+
 module.exports = router;
